b205a2669d58b2aee465e130e40fed1208ea0685
Vladimir Rastorguev <vmrmozg138@gmail.com>
2026-05-30 16:27:26 +0300

    This reverts commit c1f3d815d996423a435e4a24e234acbc7a7db429.
    qasfgsfg

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,21 @@
+from masks import *
 from datetime import datetime
 
-from masks import get_mask_account, get_mask_card_number
-
-card_names = ["Maestro", "MasterCard", "Visa Classic", "Visa Platinum", "Visa Gold"]
-
-
+card_names = ['Maestro','MasterCard','Visa Classic', 'Visa Platinum', 'Visa Gold']
 def mask_account_card(card_or_account):
     name, number = card_or_account.strip().rsplit(" ", maxsplit=1)
 
+    #print(f'name: {name}, number: {number}')
+
     if name in card_names and len(number) == 16:
         return card_or_account.replace(number, get_mask_card_number(number))
-    elif name.lower() == "счет" and len(number) == 20:
+    elif name.lower() == 'счет' and len(number) == 20:
         return card_or_account.replace(number, get_mask_account(number))
     else:
-        return "Данные введены некорректно"
+        return 'Данные введены некорректно'
 
+
+#print(mask_account_card('Счет 6468647367889477'))
 
 def get_date(date_iso):
     dt = datetime.fromisoformat(date_iso)
