c2d3768b3cd3897b6fb67fa05c762f2cc7a58138
Vladimir Rastorguev <vmrmozg138@gmail.com>
2026-05-31 13:00:34 +0300

    fixes after 1st check + docstrings

--- src/widget.py
+++ src/widget.py
@@ -17,6 +17,7 @@ def mask_account_card(card_or_account):
 
 
 def get_date(date_iso):
+    """функция для приведения даты из ISO к формату ДД.ММ.ГГГГ"""
     dt = datetime.fromisoformat(date_iso)
     formatted_date = dt.strftime("%d.%m.%Y")
     return formatted_date
