e861602552751240ca836274f9e704f6d1f74166
Vladimir Rastorguev <vmrmozg138@gmail.com>
2026-06-08 14:37:10 +0300

    corrections with types annotations

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,10 @@
-def filter_by_state(dicts_list: list[dict], state="EXECUTED") -> list[dict]:
+def filter_by_state(dicts_list: list[dict[str, str | int ]], state="EXECUTED") -> list[dict[str, str | int ]]:
     """функция сортирует поданный на вход список словарей,
-    осталвляет только те, у кого значение по ключу state совпадает с указанные во втором аргументе
+    оставляет только те, у кого значение по ключу state совпадает с указанным во втором аргументе
     (по умолчанию - EXECUTED)"""
     return list(filter(lambda d: d["state"] == state, dicts_list))
 
 
-def sort_by_date(dicts_list: list[dict], reverse=True) -> list[dict]:
+def sort_by_date(dicts_list: list[dict[str, str | int ]], reverse=True) -> list[dict[str, str | int ]]:
     """функция сортирует список словарей по дате, направление по умолчанию - по убыванию"""
     return sorted(dicts_list, key=lambda x: x["date"], reverse=reverse)
