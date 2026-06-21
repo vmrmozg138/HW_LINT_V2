14650df810a067ae72abd21d170370665b62c220
Vladimir Rastorguev <vmrmozg138@gmail.com>
2026-06-21 23:05:19 +0300

    Generators fix by linters

--- src/generators.py
+++ src/generators.py
@@ -1,122 +1,34 @@
 def filter_by_currency(lst, currency):
-    return filter(lambda x: x["operationAmount"]["currency"]["code"] == currency if ("operationAmount" in x.keys() and
-                                                                                     "currency" in x["operationAmount"].keys() and
-                                                                                     "code" in x["operationAmount"]["currency"].keys()) else None, lst)
+    return filter(
+        lambda x: (
+            x["operationAmount"]["currency"]["code"] == currency
+            if (
+                "operationAmount" in x.keys()
+                and "currency" in x["operationAmount"].keys()
+                and "code" in x["operationAmount"]["currency"].keys()
+            )
+            else None
+        ),
+        lst,
+    )
 
 
 def transaction_descriptions(lst):
-    return map(lambda x : x["description"] if "description" in x.keys() else None, lst)
+    return map(lambda x: x["description"] if "description" in x.keys() else None, lst)
+
 
 def card_number_generator(start, end):
     if end > start:
         tmpl = "xxxxxxxxxxxxxxxx"
-        res = (' '.join([(tmpl[:-len(str(i))]+str(i))[j:j+4] for j in range(0, len(tmpl), 4)])  for i in range(start, end))
+        res = (
+            " ".join(
+                [
+                    (tmpl[: -len(str(i))] + str(i))[j : j + 4]
+                    for j in range(0, len(tmpl), 4)
+                ]
+            )
+            for i in range(start, end)
+        )
         return res
     else:
-        return 'Неверно указаны границы диапазона'
-
-for card_num in card_number_generator(1,5):
-    print(card_num)
-
-
-
-transactions = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-)
-
-transactions2 = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878"
-        }
-    ]
-)
-
-for example in filter_by_currency(transactions2, "RUB"):
-    print(example)
-for example in transaction_descriptions(transactions):
-    print(example)
-
+        return "Неверно указаны границы диапазона"
