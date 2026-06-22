db9c80034d9050139c434c940d541d67dd89131b
Vladimir Rastorguev <vmrmozg138@gmail.com>
2026-06-22 15:39:32 +0300

    Generators and tests after corrections

--- src/generators.py
+++ src/generators.py
@@ -1,11 +1,12 @@
 def filter_by_currency(lst, currency):
+    """Функция для фильтрации списка входных операций по заданной валюте(если в данных нет указанной в агрументе валюты, то вернет None)"""
     return filter(
         lambda x: (
             x["operationAmount"]["currency"]["code"] == currency
             if (
-                "operationAmount" in x.keys()
-                and "currency" in x["operationAmount"].keys()
-                and "code" in x["operationAmount"]["currency"].keys()
+                x.get("operationAmount") is not None
+                and x["operationAmount"].get("currency") is not None
+                and x["operationAmount"]["currency"].get("code") is not None
             )
             else None
         ),
@@ -14,10 +15,12 @@ def filter_by_currency(lst, currency):
 
 
 def transaction_descriptions(lst):
+    """Функция для выделения только описания транзакций из полного списка транзакций"""
     return map(lambda x: x["description"] if "description" in x.keys() else None, lst)
 
 
 def card_number_generator(start, end):
+    """Функция для генерации номеров карт"""
     if end > start:
         tmpl = "xxxxxxxxxxxxxxxx"
         res = (
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -22,3 +22,59 @@ def test_transaction_descriptions(transactions_all, transactions_dscr_all):
 def test_card_number_generator_small_limits(cards4):
     result = list(card_number_generator(start=1, end=5))
     assert result == cards4
+
+
+@pytest.mark.parametrize(
+    "input_lst, currency, output",
+    [
+        (
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "operationAmount": {
+                        "amount": "9824.07",
+                        "currency": {"name": "USD"},
+                    },
+                    "description": "Перевод организации",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                }
+            ],
+            "USD",
+            [],
+        ),
+        (
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "operationAmount": {"amount": "9824.07"},
+                    "description": "Перевод организации",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                }
+            ],
+            "USD",
+            [],
+        ),
+        (
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "description": "Перевод организации",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                }
+            ],
+            "USD",
+            [],
+        ),
+    ],
+)
+def test_filter_by_currency_no_correct_data(input_lst, currency, output):
+    assert list(filter_by_currency(input_lst, currency)) == output
